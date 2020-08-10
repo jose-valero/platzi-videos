d55143203a2aa84fe077ed472a803e229f6acf79
Jose Valero <josemiguelvaleroreyes@gmail.com>
2020-08-10 13:41:32 -0300

    initial commit

---
+++ src/components/Hellow.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../assets/styles/App.scss";
+
+const Hellow = () => (
+
+  console.log("hola mundo"),
+  <h1>Hola Mundo</h1>
+);
+
+
+export default  Hellow; 
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Hellow from "./components/Hellow";
+
+ReactDOM.render(<Hellow />, document.getElementById("App"));
